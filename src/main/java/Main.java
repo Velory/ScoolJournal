1a5523e7dfcf807938c217dfbdf85481365127e2
Sirotkina <marishach@te.net.ua>
2017-03-02 01:15:45 +0200

    create DB, migration, properties

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.flywaydb.core.Flyway;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Flyway flyway = new Flyway();
+        Properties properties = new Properties();
+        properties.load(new FileReader("D:\\Java\\ScoolJournal\\src\\main\\resources\\db.properties"));
+
+        flyway.setDataSource(properties.getProperty("Flyway.url"),
+                properties.getProperty("Flyway.user"), properties.getProperty("Flyway.password"));
+
+        /*flyway.setDataSource("jdbc:mysql://localhost:3306/mydb",
+                "root", "marishach");*/
+
+        flyway.migrate();
+    }
+}
